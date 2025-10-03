5cafa288317562af4a30e9d2101be4de70b42bdd
s95rob <s95rob@gmail.com>
2025-10-02 21:29:05 -0700

    Use quantized values for fractional part initialization

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # fx
 
-Header-only fixed-point arithmetic C implementation for embedded systems without a dedicated FPU. 
+Header-only fixed-point arithmetic C implementation for embedded systems without a dedicated FPU. Leverages compiler constant folding to initialize fixed-point 
+numbers *like* floating-point. 
 
-+ Use `#define FX_NO_INLINE` to remove function inline hints. 
-+ Use `#define FX32_FRAC_BITS number_of_bits` to define the amount of bits used for the fractional part (default 8 bits).
-+ Use `#define FX32_FAST` before including this header to avoid up-casting during multiplication/division operations to mitigate overflow. 
+### Features
++ 32-bit (**Q**23.**8**, adjustable) fixed-point creation and resolution
++ Common arithmetic functions
++ Ceil and floor functions 
+
+### Example
+```c
+#include "fx.h"
+
+fx32 f = fx32_make(1, 500);             // Create an approximation of 1.5f
+f = fx32_mul(f, fx32_make(2, 0));       // Multiply it by 2.0f
+int i = fx32_resolve(f)                 // Resolve `f` to 3
+```
+
+### Macros
+
+| Macro | Usage | Example |
+|---|---|---|
+| `FX_NO_INLINE` | Remove compiler inline hints | `#define FX_NO_INLINE` |
+| `FX32_SHIFT_BITS` | Define the amount of bits used for `fx32` fractional part (default 8 bits) | `#define FX32_SHIFT_BITS 8` |
+| `FX32_QUANTIZE_LIMIT` | Define the upper limit of the fractional part's quantized range (default 1000, avoid exceeding 2^16) | `#define FX32_QUANTIZE_LIMIT 1000` |
+| `FX32_FAST` | Define before including `fx.h` to avoid mitigating overflow by up-casting during multiplication/division | `#define FX32_FAST` |
--- fx.h
+++ fx.h
@@ -2,8 +2,12 @@
 
     Use `#define FX_NO_INLINE` to remove function inline hints. 
 
-    Use `#define FX32_FRAC_BITS number_of_bits` to define the amount of bits used
+    Use `#define FX32_SHIFT_BITS number_of_bits` to define the amount of bits used
     for the fractional part (default 8 bits).
+
+    Use `#define FX32_QUANTIZE_LIMIT` to define the upper limit of the fractional
+    part's quantized range, default 1000 (affects `f` maximal in `fx32_make`, 
+    avoid exceeding 2^16). 
 
     Use `#define FX32_FAST` before including this header to avoid
     up-casting during multiplication/division operations to
@@ -21,19 +25,25 @@
     #define FX_INLINE
 #endif
 
-#ifndef FX32_FRAC_BITS
-    #define FX32_FRAC_BITS 8
+#ifndef FX32_SHIFT_BITS
+    #define FX32_SHIFT_BITS 8
 #endif
 
-#define FX32_ONE (1 << FX32_FRAC_BITS)
+#ifndef FX32_QUANTIZE_LIMIT
+    #define FX32_QUANTIZE_LIMIT 1000
+#endif
+
+#define FX32_ONE (1 << FX32_SHIFT_BITS)
+#define FX32_HALF (FX32_ONE >> 1)
 #define FX32_MASK (FX32_ONE - 1)
+#define FX32_QUANTIZE(n) ((n * ((FX32_ONE * (1 << 16)) / FX32_QUANTIZE_LIMIT)) >> 16)
 
 typedef int32_t fx32;
 
 
 // Return a fixed-point number from whole part `i` and fractional part `f`
-FX_INLINE fx32 fx32_make(int32_t i, uint16_t f) {
-    return (fx32)((i << FX32_FRAC_BITS) | f);
+FX_INLINE fx32 fx32_make(int32_t i, uint32_t f) {
+    return (fx32)((i << FX32_SHIFT_BITS) | FX32_QUANTIZE(f));
 }
 
 // Return a fixed-point number from float `f` 
@@ -45,12 +55,12 @@ FX_INLINE fx32 fx32_from_float(float f) {
 // Return a float from fixed-point number `f`
 // (slow - uses software implementation for non-FPU systems)
 FX_INLINE float fx32_to_float(fx32 f) {
-    return (float)f / FX32_ONE;
+    return ((float)f / FX32_ONE);
 }
 
-// Return a standard 32-bit (signed) integer from fixed-point number `f`
+// Return a standard 32-bit (signed) integer from fixed-point number `f` (rounded)
 FX_INLINE int32_t fx32_resolve(fx32 f) {
-    return (f >> FX32_FRAC_BITS);
+    return ((f + FX32_HALF) >> FX32_SHIFT_BITS);
 }
 
 // Add fixed-point number `b` to `a`
@@ -66,18 +76,18 @@ FX_INLINE fx32 fx32_sub(fx32 a, fx32 b) {
 // Multiply fixed-point number `a` by `b`
 FX_INLINE fx32 fx32_mul(fx32 a, fx32 b) {
     #ifdef FX32_FAST
-        return ((a * b) >> FX32_FRAC_BITS);
+        return ((a * b) >> FX32_SHIFT_BITS);
     #else
-        return (fx32)(((uint64_t)a * b) >> FX32_FRAC_BITS);
+        return (fx32)(((uint64_t)a * b) >> FX32_SHIFT_BITS);
     #endif
 }
 
 // Divide fixed-point number `a` by `b`
 FX_INLINE fx32 fx32_div(fx32 a, fx32 b) {
     #ifdef FX32_FAST
-        return ((a << FX32_FRAC_BITS) / b);
+        return ((a << FX32_SHIFT_BITS) / b);
     #else
-        return (fx32)(((uint64_t)a << FX32_FRAC_BITS) / b);
+        return (fx32)(((uint64_t)a << FX32_SHIFT_BITS) / b);
     #endif
 }
 
